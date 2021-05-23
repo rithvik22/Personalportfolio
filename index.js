1d50cec29e7e0b2122abeca77ab37e11c40a1cdc
rithvik22 <65026219+rithvik22@users.noreply.github.com>
2021-05-23 12:55:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+
+console.log("ok")
+var typed = new Typed(".tp", {
+    strings: ["Coder", "Front-End Developer", "Youtuber","CS Student"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
